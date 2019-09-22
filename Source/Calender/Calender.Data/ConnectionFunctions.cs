1e28c3df4d01f70a2eb7d3baa816df07671415d7
Aage ten Hengel <a.ten.hengel@2bmore.com>
2019-09-22 08:32:50 +0200

    Add connect function

---
+++ Source/Calender/Calender.Data/ConnectionFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using LaYumba.Functional;
+
+namespace Calender.Data
+{
+    using static F;
+
+    public static class ConnectionFunctions
+    {
+        // based on an example in: Functional Programming in C# (Enrinco Buonanno).
+        public static T Connect<T>
+            (ConnectionString connStr,
+            Func<IDbConnection, T> f)
+        {
+            return Using(
+                () => new SqlConnection(connStr), f);
+        }
+
+        public class ConnectionString
+        {
+            readonly string value;
+
+            private ConnectionString(string value)
+            {
+                this.value = value;
+            }
+
+            public static implicit operator ConnectionString(string s)
+                => new ConnectionString(s);
+
+            public static implicit operator string(ConnectionString c)
+                => c.value;
+        }
+    }
+}
