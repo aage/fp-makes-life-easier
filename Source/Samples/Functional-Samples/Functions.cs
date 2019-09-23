0e27e7f75e530c1d773f3ac2e63912a026f01cd9
Aage ten Hengel <a.ten.hengel@2bmore.com>
2019-09-23 15:46:07 +0200

    Add samples

---
+++ Source/Samples/Functional-Samples/Functions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Functional_Samples
+{
+    public static class Functions
+    {
+        static Dictionary<int, int> map =
+            new Dictionary<int, int>
+            {
+                { 1, 2 },
+                { 2, 3 },
+                { 3, 4 },
+                { 4, 5 }
+                // ...
+            };
+
+        // value x to value y
+        public static int PlusOne(int x) => map[x];
+
+        // type A to type B
+        public static Func<int, string> AsNumber = (num) => num.ToString();
+
+        // This is a dishonest function; it says it will return an int when you give it a string
+        // it not only won't do that but it'll also throw exceptions.
+        public static Func<string, int> ParseDishonest = int.Parse;
+
+        // This is more honest (but uses a specific type)
+        public static ParseIntResult ParseHonest(this string s)
+        {
+            int i;
+            return int.TryParse(s, out i)
+                ? new ParseIntResult(result: i)
+                : new ParseIntResult(success: false);
+        }
+    }
+}
---
+++ Source/Samples/Functional-Samples/ParseIntResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Functional_Samples
+{
+    public class ParseIntResult
+    {
+        public ParseIntResult(int result = 0, bool success = true)
+        {
+            this.Result = result;
+            this.Success = true;
+        }
+
+        public int Result { get; }
+        public bool Success { get; }
+    }
+}
